da2e311f9e88dca2e4c6e321710ff96a635cc250
Alexandra Ash <alexandraleah@gmail.com>
2018-09-28 12:17:20 -0400

    add wepback dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
+const path = require('path')
+
 module.exports = {
   mode: 'development',
   entry: ['@babel/polyfill', './src/index.js'],
   output: {
-    path: __dirname + '/public',
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   resolve: {
     extensions: ['.js']
   },
   devtool: 'source-map',
+  devServer: {
+    contentBase: './public'
+  },
   node: {
     fs: 'empty'
   }
