4d4f4f524ba3a1ae12152d0c7f49e44be3f07dc2
Alexandra Ash <alexandraleah@gmail.com>
2018-09-29 16:43:08 -0400

    testing: trying to write tests

--- src/calculate.js
+++ src/calculate.js
@@ -4,8 +4,8 @@ import area from '@turf/area' //this is used to calculate the area of the polygo
 //exports the function for use in the capturePolygon module
 export default function calculatePower(data) {
   //calculates only if the length is greater than  zero
-  let caclualatedArea = Math.round(area(data)) //uses turf area to calculate area of the polygon and round to nearest meter
+  let calculatedArea = Math.round(area(data)) //uses turf area to calculate area of the polygon and round to nearest meter
   //calculate the nominal power assuming 150 Watts per meter, round to the nearest watt and convert to kW
-  let power = Math.round(caclualatedArea * 150) / 1000
-  return {caclualatedArea, power}
+  let power = Math.round(calculatedArea * 150) / 1000
+  return {calculatedArea, power}
 }
--- src/tests.spec.js
+++ src/tests.spec.js
@@ -1,7 +1,17 @@
 const calculatePower = require('./calculate')
+const {expect} = require('chai')
+let calculate = calculatePower.calculatePower
+
+const data = [[125, -15], [113, -22], [154, -27], [144, -15], [125, -15]]
 
 describe('calculatePower', function() {
-  it('returns an array', function() {
-    expect(calculatePower()).to.be.an('array')
+  it('is a function', function() {
+    expect(calculate).to.be.an('function')
+  })
+  it('returns an object', function() {
+    expect(calculate(data)).to.be.an('object')
+  })
+  it('calculates the area', function() {
+    expect(calculate(data).calculatedArea).to.be.an('integer')
   })
 })
